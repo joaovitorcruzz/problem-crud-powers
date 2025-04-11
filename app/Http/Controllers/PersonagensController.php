d88f63213c752b43efeecda8cfd86cf07dcb817f
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-04-11 00:05:22 -0300

    feat: alter column and create validation on post persona to don`t catch errors without example

--- app/Http/Controllers/PersonagensController.php
+++ app/Http/Controllers/PersonagensController.php
@@ -21,15 +21,30 @@ class PersonagensController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
+    {   
+        if ($request->name == null) {
+            return response()->json('Nome inválido, insira outro!', 400);
+        }
+
+        if ($request->nickname == null) {
+            return response()->json('Apelido inválido, insira outro!', 400);
+        }
+
+        if (!in_array($request->class, ["Guerreiro", "Mago", "Arqueiro", "Ladino", "Bardo"])) {
+            return response()->json('Classe inválida, insira outra!',400);
+        } 
+       
+        if (($request->strength + $request->defence) > 10) {
+            return response()->json('A soma de força e defesa não pode ser maior que 10! sejá decisivo agora, pois seus futuros items magicos serão influencia nessa soma também!', 400);
+        }
+
         $personagem = new Personagens();
         $personagem->name = $request->name;
         $personagem->nickname = $request->nickname;
-        $personagem->class = $request->class;
-        $personagem->level = $request->level;
-        $personagem->magic_item_id = $request->magic_item_id;
-        $personagem->strength = $request->strength;
-        $personagem->defence = $request->defence;
+        $personagem->class = $request->class ?? "Guerreiro";
+        $personagem->level = $request->level ?? 1;
+        $personagem->strength = $request->strength ?? 0;
+        $personagem->defence = $request->defence ?? 0;
         $personagem->save();
 
         return response()->json($personagem, 201);
@@ -55,7 +70,6 @@ class PersonagensController extends Controller
             $personagem->nickname = $request->nickname;
             $personagem->class = $request->class;
             $personagem->level = $request->level;
-            $personagem->magic_item_id = $request->magic_item_id;
             $personagem->strength = $request->strength;
             $personagem->defence = $request->defence;
             $personagem->save();
