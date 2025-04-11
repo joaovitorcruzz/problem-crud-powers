243ce8a72cf0c109a4ca5b4a2224c0d38c75137a
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-04-11 00:22:28 -0300

    feat: create a validation on magic items, using type, sum`s and conditional

--- app/Http/Controllers/MagicItemsController.php
+++ app/Http/Controllers/MagicItemsController.php
@@ -22,11 +22,34 @@ class MagicItemsController extends Controller
      */
     public function store(Request $request)
     {
+        if (!in_array($request->type, ["Arma", "Armadura", "Amuleto"])) {
+            return response()->json('Tipo inválido, insira outro!',400);
+        } 
+        
+        $defence = $request->defence ?? 0;
+        $strength = $request->strength ?? 0;
+
+        if ($request->type == "Arma"){
+            $defence = 0;
+        }
+        
+        if ($request->type == "Armadura"){
+            $strength = 0;
+        }
+
+        if ($strength == 0 && $defence == 0) {
+            return response()->json('A força e defesa não podem ser 0!', 400);
+        }
+
+        if(($strength + $defence) > 10){
+            return response()->json('A soma de força e defesa não pode ser maior que 10! ou individualmente!', 400);
+        }
+        
         $magic_items = new MagicItems();
         $magic_items->name = $request->name;
         $magic_items->type = $request->type;
-        $magic_items->strength = $request->strength;
-        $magic_items->defence = $request->defence;
+        $magic_items->strength = $strength;
+        $magic_items->defence = $defence;
         $magic_items->save();
 
         return response()->json($magic_items, 201);
