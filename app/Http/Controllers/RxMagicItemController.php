cd719f60e6e47422bd307484afa65fa2818ffcb0
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2025-04-11 01:28:49 -0300

    feat: finishing the project with all cruds and conditional validations and rename mistake labels

--- app/Http/Controllers/RxMagicItemController.php
+++ app/Http/Controllers/RxMagicItemController.php
@@ -18,16 +18,16 @@ class RxMagicItemController extends Controller
         $rxMagicItem = RxMagicItem::where('personagem_id',$personagem_id)->pluck('magic_item_id');
 
         if ($rxMagicItem->isEmpty()) {
-            return response('Esse personagem não possui nenhum item mágico!', 400);
+            return response()->json('Esse personagem não possui nenhum item mágico!', 400);
         }
 
         $magicItem = MagicItems::whereIn('id', $rxMagicItem)->get();
 
         if ($magicItem->isEmpty()) {
-            return response('Esse personagem não possui nenhum item mágico!', 400);
+            return response()->json('Esse personagem não possui nenhum item mágico!', 400);
         }
 
-        return response($magicItem, 200);
+        return response()->json($magicItem, 200);
     }
     
     /**
@@ -40,7 +40,7 @@ class RxMagicItemController extends Controller
 
         $checkDuplicateWithoutUnique = RxMagicItem::where('magic_item_id', $magicItem->id)->where('personagem_id', $personagem->id)->first();
         if($checkDuplicateWithoutUnique){
-            return response('O personagem já possui esse item mágico!', 400);
+            return response()->json('O personagem já possui esse item mágico!', 400);
         }
 
         if ($magicItem->type == "Amuleto") {
@@ -49,7 +49,7 @@ class RxMagicItemController extends Controller
     
             foreach ($magicItemsByPersona as $item) {
                 if ($item->type == "Amuleto") {
-                    return response('O personagem já possui um amuleto!', 400);
+                    return response()->json('O personagem já possui um amuleto!', 400);
                 }
             }
         }
@@ -59,7 +59,7 @@ class RxMagicItemController extends Controller
         $rxMagicItem->personagem_id = $personagem->id;
         $rxMagicItem->save();
 
-        return response($rxMagicItem, 201);
+        return response()->json($rxMagicItem, 201);
     }
 
     /**
@@ -67,8 +67,39 @@ class RxMagicItemController extends Controller
      */
     public function service_item_persona_to_remove($personagem_id, $magic_item_id)
     {
-        $rxMagicItemToDelete = RxMagicItem::where('magic_item_id', $magic_item_id)->where('personagem_id', $personagem_id)->delete();
-    
-        return response('Magia removida do Personagem', 200);
+        $rxMagicItemToDelete = RxMagicItem::where('magic_item_id', $magic_item_id)->where('personagem_id', $personagem_id)->first();
+        
+        if ($rxMagicItemToDelete == null) {
+            return response()->json('Esse personagem não possui esse item mágico!', 400);
+        }
+
+        $rxMagicItemToDelete->delete();
+
+        return response()->json('Magia removida do Personagem', 200);
     }    
+
+    public function service_item_persona_amuleto($personagem_id)
+    {
+
+        $rxMagicItem = RxMagicItem::where('personagem_id',$personagem_id)->pluck('magic_item_id');
+
+        if($rxMagicItem->isEmpty()){
+            return response()->json('Esse personagem não possui nenhum item mágico!', 400);
+        }
+
+        $magicItem = MagicItems::whereIn('id', $rxMagicItem)->get();
+
+        if($magicItem->isEmpty()){
+            return response()->json('Esse personagem não possui nenhum item mágico!', 400);
+        }
+
+        foreach($magicItem as $item){
+            if($item->type == "Amuleto"){
+                return response()->json($item, 200);
+            }
+        }
+
+        return response()->json('Esse personagem não possui nenhum amuleto!', 400);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ Route::resource('magic-items', MagicItemsController::class);
 Route::get('service-item-persona/{personagem_id}', [RxMagicItemController::class, 'service_item_persona']);
 Route::post('service-item-persona/{personagem_id}/to-add/{magic_item_id}', [RxMagicItemController::class, 'service_item_persona_to_add']);
 Route::delete('service-item-persona/{personagem_id}/to-remove/{magic_item_id}', [RxMagicItemController::class, 'service_item_persona_to_remove']);
+Route::get('service-item-persona-amuleto/{personagem_id}', [RxMagicItemController::class, 'service_item_persona_amuleto']);
